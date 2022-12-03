0571590e1895cb7aadc64787af1a6db05813d033
Axtraix <98992939+Axtraix@users.noreply.github.com>
2022-12-03 17:47:40 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ left_wrist_x=0
 left_wrist_y=0
 right_wrist_x=0
 right_wrist_y=0
+score_leftwrist=0
+score_rightwrist=0
+var status_songs=""
 function preload(){
 song1=loadSound("metamorphosis.mp4")
 song2=loadSound("レプリカント.mp4")
@@ -18,6 +21,22 @@ posenet.on('pose',gotPoses)
 }
 function draw(){
 image(video,0,0,600,400)
+fill("blue")
+stroke("blue")
+if(score_leftwrist>0.002){
+    circle(left_wrist_x,left_wrist_y,20)
+    if(song2.isPlaying()==true){
+        song2.stop()
+        song1.play()
+    }
+}
+if(score_rightwrist>0.002){
+    circle(right_wrist_x,right_wrist_y,20)
+    if(song1.isPlaying()==true){
+        song2.stop()
+        song1.play()
+    }
+}
 }
 function modelLoaded(){
 console,log("Posenet has been initialized successfully")
@@ -28,5 +47,7 @@ left_wrist_x=results[0].pose.leftWrist.X;
 right_wrist_x=results[0].pose.rightWrist.X;
 left_wrist_x=results[0].pose.leftWrist.Y;
 right_wrist_x=results[0].pose.rightWrist.Y;
+score_leftwrist=results[0].pose.keypoints[9].score
+score_rightwrist=results[0],pose.keypoints[10].score
 }
 }
